eda62abb4d9836888dfee806fd207b1e3bad8bbf
Claire <cet6ax@virginia.edu>
2016-02-10 00:10:00 -0500

    Added lifecycle methods to the MainActivity

--- app/src/main/java/com/example/cs4720/uvabucketlist/MainActivity.java
+++ app/src/main/java/com/example/cs4720/uvabucketlist/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.cs4720.uvabucketlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -10,10 +16,21 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
 
 
+    public ArrayList<CheckBox> itemList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startInfoActivity(v);
+            }
+        });
+
     }
 
 
@@ -40,5 +57,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop(){
         super.onStop();
+    }
+
+    public void startInfoActivity(View view){
+        Intent intent = new Intent(this, InfoActivity.class);
+        startActivity(intent);
     }
 }
